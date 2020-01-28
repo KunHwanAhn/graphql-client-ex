94489e3702c0f0240c4d0502ddf787f68bd67195
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-01-28 18:57:14 +0900

    [directive] v-loading directive 추가

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import apolloProvider from './plugins/vueApollo'
+import loading from './directives/loading'
 import '@/styles/index.scss'
 
 Vue.config.productionTip = false
+Vue.directive('loading', loading)
 
 new Vue({
   router,
